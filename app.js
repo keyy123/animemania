043f240967d49ef6b527211bea1225e69d608123
keyy123 <parktwins1@gmail.com>
2022-11-16 16:22:45 -0500

    chore

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const nextBtns = document.querySelectorAll(".next-btn");
 const allInputs = document.querySelectorAll("input[type='text']")
 
+function thisDOMElement(string) {
+    let element = document.querySelector(string);
+    if (element === null) {
+        console.error(new Error("Not a valid HTML element"))
+    }
+    return element;
+}
+
 let input, secondInput, num;
 nextBtns.forEach((btn) => {
     btn.setAttribute("type", "button");
@@ -17,15 +25,30 @@ nextBtns.forEach((btn) => {
 
 
 function showNextQuestion(number) {
-    let inputText = document.querySelector(`.question-${number} input[type='text']`);
-    if (inputText.value <= 5) {
+    // let inputText = document.querySelector(`.question-${number} input[type='text']`);
+    let inputText = thisDOMElement(`.question-${number} input[type='text']`);
+
+    if (inputText.value.length <= 5) {
+        console.log(inputText.value)
         return;
     }
 
-    input = document.querySelector(`.question-${number}`);
-    secondInput = document.querySelector(`.question-${number + 1}`);
-    console.log(input, secondInput, inputText);
-    input.style = "display:none;";
-    secondInput.style = "display:block;";
+    input = thisDOMElement(`.question-${number}`);
+    secondInput = thisDOMElement(`.question-${number + 1}`);
+    // let nextQuestion = document.querySelector(`.question-${number + 2}`);
+    // console.log(input, secondInput);
+
+    if (secondInput) {
+        input.style = "display:none;";
+        secondInput.style = "display:block;";
+    }
 }
+
+nextBtns[2].addEventListener("click", () => {
+    thisDOMElement('div.question-box').style.display = "none";
+    thisDOMElement('main').textContent = "Loading..."
+    setTimeout(() => {
+        thisDOMElement('main').textContent = "Done!"
+    }, 3000);
+});
 
