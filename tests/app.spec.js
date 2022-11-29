267b8d59add770e068dd7df6e07e2378b187dbdf
keyy123 <parktwins1@gmail.com>
2022-11-28 21:51:16 -0500

    skill: Tested dark mode, learned of window global prop to see CSS styles, learned that it is not good to use unit testing framework like jest totest if CSS styles are applied to HTML document. Use cypress or something similar for that!

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -10,6 +10,11 @@ describe(" toggle dark mode", () => {
     test("adding dark mode class", () => {
         toggleDarkMode();
         expect(window.document.body.className).toBe("dark-mode");
-    })
+    });
 });
 
+/*
+It is better to use testing frameworks like Cypress to confirm if
+the CSS styles applied to an HTML element. It is not best practice
+for a unit test to do so.
+*/
